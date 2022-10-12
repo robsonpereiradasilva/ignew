db981cf69ecc05359f57d359d5f127937291a489
robsonpereiradasilva <robson.silva@novaandradina.org>
2022-10-12 12:52:57 -0400

    Validate whether customer exists on stripe

--- src/pages/SubscribeButton/index.tsx
+++ src/pages/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import styles from './styles.module.scss';
 import { useSession, signIn } from 'next-auth/react'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 interface SubscribeButtonProps {
     priceId: string;
@@ -8,10 +10,23 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const { data: session } = useSession();
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (!session) {
             signIn('github')
             return;
+        }
+
+        try{
+            const response = await api.post('/subscribe')
+            
+            const {sessionId} = response.data;
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({sessionId})
+
+        }catch(error){
+            alert(error.message)
         }
     }
     return (
