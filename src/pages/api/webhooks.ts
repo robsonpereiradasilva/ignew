453f18b38013aaf47d840dd477061a5c5d8cad71
robsonpereiradasilva <robson.silva@novaandradina.org>
2022-10-13 18:34:08 -0400

    Improve webhooks api

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream'
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
     const chunks = []
@@ -21,7 +22,9 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    'customer.subscription.updated',
+    'customer.subscription.delete',
 ])
 
 
@@ -34,22 +37,40 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         let event: Stripe.Event;
 
         try {
-            event: stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+            event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
         } catch (error) {
             return res.status(400).send(`Webhook erro: ${error.message}`);
         }
 
-        const type = event.type;
+        const { type } = event;
 
 
         if (relevantEvents.has(type)) {
             try {
-
                 switch (type) {
-                    case 'checkout.session.completed':
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.delete':
+
+                        const subscription = event.data.object as Stripe.Subscription;
+
+                        
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            false
+                            )                            
+                        break;
+
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session
+                        
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString(),
+                            true
+                        )
                         break;
                     default:
-                        throw new Error('Unhandled event.')
+                    throw new Error('Unhandled event.')
                 }
             }
             catch (error) {
